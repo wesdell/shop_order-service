1a5b30c3c8fb11a54b5ec13ee35a669762f12c0c
Sebastian Guerrero <wesdell17@gmail.com>
2026-03-04 14:32:03 -0500

    Check inventory before placing an order

--- src/main/java/com/wesdell/shop/order/service/OrderService.java
+++ src/main/java/com/wesdell/shop/order/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.wesdell.shop.order.service;
 
 import com.wesdell.shop.order.DTO.OrderRequest;
+import com.wesdell.shop.order.client.InventoryClient;
 import com.wesdell.shop.order.model.Order;
 import com.wesdell.shop.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +14,14 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void createOrder(OrderRequest newOrder) {
+        boolean isInStock = inventoryClient.isItemInStock(newOrder.skuCode(), newOrder.quantity());
+        if (!isInStock) {
+            throw new RuntimeException("Product: "  + newOrder.skuCode() + " not exists or there are not enough items in stock");
+        }
+
         Order order = orderRequestToOrder(newOrder);
         order.setOrderNumber(UUID.randomUUID().toString());
         orderRepository.save(order);
