f232d116128ef4c019fd8e81a47725dc8ec11709
Sebastian Guerrero <wesdell17@gmail.com>
2026-03-04 14:31:40 -0500

    Create feign client to send requests to the inventory service

---
+++ src/main/java/com/wesdell/shop/order/client/InventoryClient.java
@@ -0,0 +1,13 @@
+package com.wesdell.shop.order.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "inventory", url = "http://localhost:8082")
+public interface InventoryClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    boolean isItemInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+}
