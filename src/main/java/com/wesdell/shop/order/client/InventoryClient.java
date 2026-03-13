ea4e8834298f9391b287b028ae289af559fc125b
Sebastian Guerrero <wesdell17@gmail.com>
2026-03-13 15:09:26 -0500

    Implement circuit breaker in order service

--- src/main/java/com/wesdell/shop/order/client/InventoryClient.java
+++ src/main/java/com/wesdell/shop/order/client/InventoryClient.java
@@ -1,10 +1,23 @@
 package com.wesdell.shop.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name= "inventory", fallbackMethod = "callFallbackMethod")
+    @Retry(name = "inventory")
     boolean isItemInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean callFallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode {}, failure reason {}", skuCode, throwable.getMessage());
+        return false;
+    }
 }
