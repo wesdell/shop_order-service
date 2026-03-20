ccf987952adf288400cdd700b09e465a6d6761f8
Sebastian Guerrero <wesdell17@gmail.com>
2026-03-19 20:54:29 -0500

    Implement order event to send a notification when an order is created

---
+++ src/main/java/com/wesdell/shop/order/event/OrderEvent.java
@@ -0,0 +1,14 @@
+package com.wesdell.shop.order.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+
+    private String orderNumber;
+    private String email;
+}
--- src/main/java/com/wesdell/shop/order/service/OrderService.java
+++ src/main/java/com/wesdell/shop/order/service/OrderService.java
@@ -2,9 +2,11 @@ package com.wesdell.shop.order.service;
 
 import com.wesdell.shop.order.DTO.OrderRequest;
 import com.wesdell.shop.order.client.InventoryClient;
+import com.wesdell.shop.order.event.OrderEvent;
 import com.wesdell.shop.order.model.Order;
 import com.wesdell.shop.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,6 +17,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
 
     public void createOrder(OrderRequest newOrder) {
         boolean isInStock = inventoryClient.isItemInStock(newOrder.skuCode(), newOrder.quantity());
@@ -25,6 +28,9 @@ public class OrderService {
         Order order = orderRequestToOrder(newOrder);
         order.setOrderNumber(UUID.randomUUID().toString());
         orderRepository.save(order);
+
+        OrderEvent orderEvent = new OrderEvent(order.getOrderNumber(), newOrder.userDetails().email());
+        kafkaTemplate.send("order-created", orderEvent);
     }
 
     private Order orderRequestToOrder(OrderRequest newOrder) {
